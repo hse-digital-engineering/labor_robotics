fb41f29953ca131d85dcd6b831c2ab0d0df84ae1
Marco Dittmann <m.dittmann@varomo.de>
2024-12-11 06:42:09 +0100

    marker search example

--- examples/video/camera_stream/aruco_walk.py
+++ examples/video/camera_stream/aruco_walk.py
@@ -1,45 +1,30 @@
 """
-Robot Video Feed and Movement Control Script
+Robot Control Script for WebRTC and Marker Detection
 Based on: https://github.com/legion1581/go2_webrtc_connect
 
-Author: Marco Dittmann
+Author: Marco Dittmann  
 Date: 4.12.24  
 
 Description:  
-This script connects to a robotic dog and provides real-time video feed visualization.
-The robot's movement can be controlled via keyboard inputs, allowing the user to navigate and perform predefined actions.
-It uses OpenCV to display the video feed and asyncio for asynchronous robot communication.
-
-Commands:  
-- Movement Controls  
-  - w Move forward  
-  - s: Move backward  
-  - a: Rotate counterclockwise (CCW)  
-  - d: Rotate clockwise (CW)  
-  - q: Sidestep left  
-  - e: Sidestep right  
-
-- Action Controls  
-  - 1: Paw wave  
-  - 2: Sit  
-  - 3: Stand down  
-  - 4: Stand up  
+This script uses the Dog class to control the movement of a robotic dog and utilizes its camera feed to search for and detect ArUco markers.
+The robot's behavior dynamically adapts based on marker detection, allowing it to perform various actions, such as moving, sitting, and standing.  
+The script integrates OpenCV for video processing, asyncio for asynchronous communication, and WebRTC for establishing a connection with the robot.
+It processes video frames in real-time to detect markers and triggers appropriate actions based on the detection state.  
 
 Features:  
-- Real-time video feed display from the robot's camera.  
-- Keyboard-controlled robot movement and actions.  
-- Asynchronous WebRTC communication for controlling robot operations.  
-
-Requirements:  
-- Correctly set the IP address of the robot in the IP_ADDRESS constant.  
-- Install required dependencies: OpenCV, asyncio, aiortc, and go2_webrtc_driver.  
+- Real-time video streaming and marker detection with OpenCV and ArUco.  
+- Robot movement control (e.g., move, sit, stand) using the Dog class.  
+- Asynchronous communication with the robot via WebRTC.  
+- Integration with threading for smooth operation.  
 
 Note:  
-- Ensure the robot is connected and ready for remote operation before running the script.  
-- Use appropriate safety measures to prevent damage or collisions during operation.  
+- Ensure the robot's IP address is correctly set in the IP_ADDRESS constant.  
+- Install all dependencies, including OpenCV, asyncio, and aiortc.  
 """
 
+
 import cv2
+from cv2 import aruco
 import numpy as np
 
 # Create an OpenCV window and display a blank image
@@ -52,19 +37,20 @@ import asyncio
 import logging
 import threading
 import time
+from datetime import datetime
 from queue import Queue
 
 ## imports for movement
-from go2_webrtc_driver.constants import RTC_TOPIC, SPORT_CMD
+from go2_webrtc_driver.constants import RTC_TOPIC, SPORT_CMD, VUI_COLOR
 from go2_webrtc_driver.webrtc_driver import Go2WebRTCConnection, WebRTCConnectionMethod
 from aiortc import MediaStreamTrack
-import keyboard
+
 
 # Enable logging for debugging
 logging.basicConfig(level=logging.FATAL)
 
 IP_ADDRESS = "192.168.0.197"
-V_MAX = 2.0     # Maximum translational velocity (m/s)
+V_MAX = 1.0     # Maximum translational velocity (m/s)
 W_MAX = 0.8     # Maximum rotational velocity (rad/s)
 
 
@@ -75,12 +61,31 @@ class Dog:
         self.vy = 0.0
         self.vz = 0.0
         self.conn = None
+        self.marker_detected = False
+        self.search_active = False
+        self.last_detection_timestamp = 0
 
     def set_velocity(self, vx: float, vy: float, vz: float):
         self.vx = vx
         self.vy = vy
         self.vz = vz
 
+
+    async def set_vui(self, color):
+        if not self.conn:
+            logging.warning("Connection not established. Cannot perform movement.")
+            return
+
+        logging.info("Setting VUI color...")
+        if True:
+            await self.conn.datachannel.pub_sub.publish_request_new(
+                RTC_TOPIC["VUI"], {"api_id": 1007, 
+                    "parameter": 
+                    {
+                        "color": color,
+                    }
+                }
+            )
 
     async def paw_wave(self):
         if not self.conn:
@@ -144,13 +149,24 @@ class Dog:
             )
         logging.info(f"Moving robot: vx={self.vx}, vy={self.vy}, vz={self.vz}")
 
+    async def find_marker(self):
+        if (time.time() - self.last_detection_timestamp) > 3.0 and self.search_active:
+            self.set_velocity(0.0, 0.0, W_MAX)
+        else:
+            self.set_velocity(0.0, 0.0, 0.0)
+        await self.move_xyz()
 
-dog = Dog()
+dog = Dog(IP_ADDRESS)
 
 
 def main():
     global dog
     frame_queue = Queue()
+
+    aruco_dictionary = aruco.getPredefinedDictionary(aruco.DICT_4X4_50)
+    detector_parameters = aruco.DetectorParameters()
+    detector = aruco.ArucoDetector(aruco_dictionary, detector_parameters)
+    marker_size = 0.15 # 150 mm
 
     # Choose a connection method (uncomment the correct one)
     dog.conn = Go2WebRTCConnection(WebRTCConnectionMethod.LocalSTA, ip=dog.ip_address)
@@ -170,14 +186,16 @@ def main():
                 # Connect to the device
                 await dog.conn.connect()
 
-                # Start the keypress listener
-                #asyncio.create_task(keypress_listener())
-
                 # Switch video channel on and start receiving video frames
                 dog.conn.video.switchVideoChannel(True)
 
                 # Add callback to handle received video frames
                 dog.conn.video.add_track_callback(recv_camera_stream)
+
+                logging.info("Performing 'StandUp' movement...")
+                await dog.conn.datachannel.pub_sub.publish_request_new(
+                    RTC_TOPIC["SPORT_MOD"], {"api_id": SPORT_CMD["BalanceStand"]}
+                )
             except Exception as e:
                 logging.error(f"Error in WebRTC connection: {e}")
 
@@ -192,51 +210,39 @@ def main():
     asyncio_thread = threading.Thread(target=run_asyncio_loop, args=(loop,))
     asyncio_thread.start()
 
-    # Start the keypress listener in a separate thread
-    # keypress_thread = threading.Thread(target=keypress_listener, daemon=True)
-    # keypress_thread.start()
-
-
     try:
+        find_marker = True
         while True:
             if not frame_queue.empty():
                 img = frame_queue.get()
-                
+                img_greyscale = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+                corners, ids, rejected = detector.detectMarkers(img_greyscale)
+                if ids is not None:
+                    aruco.drawDetectedMarkers(img, corners, ids)
+                    dog.marker_detected = True
+                    dog.last_detection_timestamp = time.time()
+                    asyncio.run_coroutine_threadsafe(dog.set_vui(VUI_COLOR.GREEN), loop)
+
+                else:
+                    dog.marker_detected = False
+                    asyncio.run_coroutine_threadsafe(
+                        dog.set_vui(VUI_COLOR.BLUE), loop)
+                    
+                    
                 # Display the frame
                 cv2.imshow('Video', img)
                 key_input = cv2.waitKey(1)
 
-                if key_input == ord('w'):
+                if key_input == ord('1'):
                     # Move forward
-                    dog.set_velocity(V_MAX, 0.0, 0.0)
-                elif key_input == ord('a'):
-                    # Rotate counter-clockwise
-                    dog.set_velocity(0.0, 0.0, W_MAX)  
-                elif key_input == ord('s'):
-                    # Move backward
-                    dog.set_velocity(-V_MAX, 0.0, 0.0)  
-                elif key_input == ord('d'):
-                    # Rotate clockwise
-                    dog.set_velocity(0.0, 0.0, -W_MAX)  
-                elif key_input == ord('q'):
-                    # Move right
-                    dog.set_velocity(0.0, 0.5, 0.0)
-                elif key_input == ord('e'):
-                    # Move left
-                    dog.set_velocity(0.0, -0.5, 0.0)
-                elif key_input == ord('1'):
-                    asyncio.run_coroutine_threadsafe(dog.paw_wave(), loop)
-                elif key_input == ord('2'):
-                    asyncio.run_coroutine_threadsafe(dog.sit(), loop)
-                elif key_input == ord('3'):
-                    asyncio.run_coroutine_threadsafe(dog.stand_down(), loop)
-                elif key_input == ord('4'):
-                    asyncio.run_coroutine_threadsafe(dog.stand_up(), loop)
-                else:
-                    # Stop movement
-                    dog.set_velocity(0.0, 0.0, 0.0)
+                    if dog.search_active is False:
+                        dog.search_active = True
+                        print("Search active")
+                    else:
+                        dog.search_active = False
+                        print("Search inactive")
 
-                asyncio.run_coroutine_threadsafe(dog.move_xyz(), loop)
+                asyncio.run_coroutine_threadsafe(dog.find_marker(), loop)
 
             else:
                 # Sleep briefly to prevent high CPU usage
